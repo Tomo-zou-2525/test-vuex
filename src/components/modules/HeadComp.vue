4fd4b3e2fe5d90eabc4dfbeeff5b8d8f9a95d8b7
友造-2525 <bathbook1990tomozou@gmail.com>
2021-03-22 01:15:16 +0900

    HaedComp 追加

---
+++ src/components/modules/HeadComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "headComp",
+  data() {
+    return {
+      title: "感想を入力"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/modules/TextareaComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Formページ</div>
+</template>
+
+<script>
+export default {
+  name: "form",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
